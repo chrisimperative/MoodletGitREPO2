49b05203048e1d4650faa6e37ae34ba34761375a
chrisimperative <38993314+chrisimperative@users.noreply.github.com>
2021-07-26 11:02:46 -0500

    Update 3

--- MoodletGitREPO2/ViewController.swift
+++ MoodletGitREPO2/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+let sadQuoteList = ["Sometimes, life will kick you around, but sooner or later, you realize you’re not just a survivor. You’re a warrior, and you’re stronger than anything life throws your way.", "On particularly rough days when I’m sure I can’t possibly endure, I like to remind myself that my track record for getting through bad days so far is 100%, and that’s pretty good."]
 
 }
 
